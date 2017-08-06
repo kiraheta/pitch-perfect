851c1e494fe0eb9e847b750b345919162236ca7f
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-08-05 20:25:36 -0700

    Remove viewWillAppear()

--- PitchPerfect/PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/PitchPerfect/RecordSoundsViewController.swift
@@ -22,11 +22,6 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         stopRecordingButton.isEnabled = false
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("viewWillAppear called")
-    }
-
     @IBAction func recordAudio(_ sender: Any) {
         recordingLabel.text = "Recording in Progress"
         stopRecordingButton.isEnabled = true
